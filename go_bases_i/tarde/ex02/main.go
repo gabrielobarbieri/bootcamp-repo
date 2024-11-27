a69c77d7ab6bad6ee5f1a23bb3aa1e3dfaa4a5ea
gabrielobarbieri <gabriel.obarbieri@mercadolivre.com>
2024-11-27 17:25:40 -0300

    Fix ex02

--- go_bases_i/tarde/ex02/main.go
+++ go_bases_i/tarde/ex02/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func emprestimo(idade int, tempoEmprego int, salario int) {
-  if idade > 22 && tempoEmprego > 1 && salario > 100000 {
-    fmt.Println("Empréstimo liberado!")
+  if idade > 22 && tempoEmprego > 1 {
+    if salario > 100000 {
+      fmt.Println("Empréstimo liberado sem juros!")
+    } else {
+      fmt.Println("Empréstimo liberado com juros!")   
+    }
   } else { 
     fmt.Println("Empréstimo recusado!")
   }
@@ -18,6 +22,11 @@ func main() {
   emprestimo(idade, tempoEmprego, salario)
 
   idade = 20
+
+  emprestimo(idade, tempoEmprego, salario)
+
+  idade = 23
+  salario = 90000
 
   emprestimo(idade, tempoEmprego, salario)
 }
