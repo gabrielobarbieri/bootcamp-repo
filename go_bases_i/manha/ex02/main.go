0554dea6d29445716113d1aa941fbe0e706ac1a3
gabrielobarbieri <gabriel.obarbieri@mercadolivre.com>
2024-11-27 11:02:57 -0300

    ex 2

---
+++ go_bases_i/manha/ex02/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  temperatura := "30°C"
+  umidade := "37%"
+  pressao := 1020 
+
+  fmt.Printf("Temperatura: %s\n", temperatura)
+  fmt.Printf("Umidade: %s\n", umidade)
+  fmt.Printf("Pressão: %d\n", pressao)
+}
