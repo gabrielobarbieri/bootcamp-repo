da3eddba19c75821f0466387aad8fe0a76511a8f
gabrielobarbieri <gabriel.obarbieri@mercadolivre.com>
2024-11-27 15:49:33 -0300

    ex 3 tarde

---
+++ go_bases_i/tarde/ex03/README.md
@@ -0,0 +1,11 @@
+# Exercício 3 - Qual mês corresponde a
+
+
+### Crie um aplicativo que receba uma variável com o número do mês.
+
+1. Dependendo do número, imprima o mês correspondente no texto.
+2. Você consegue pensar em maneiras diferentes de resolver isso? Qual delas você escolheria e por quê?
+Ex: 7, Julio.
+    Switch, Hashmap and if-else
+
+👀Observação: verifique se o número do mês está correto.
---
+++ go_bases_i/tarde/ex03/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func printaMes(mes int) string {
+    switch mes {
+    case 1:
+        return "Janeiro"
+    case 2:
+        return "Fevereiro"
+    case 3:
+        return "Março"
+    case 4:
+        return "Abril"
+    case 5:
+        return "Maio"
+    case 6:
+        return "Junho"
+    case 7:
+        return "Julho"
+    case 8:
+        return "Agosto"
+    case 9:
+        return "Setembro"
+    case 10:
+        return "Outubro"
+    case 11:
+        return "Novembro"
+    case 12:
+        return "Dezembro"
+    default:
+        return "Mês inválido"
+    }
+}
+
+func main() { 
+  mes := 5
+
+  fmt.Println(printaMes(mes))
+
+  mes = 8
+
+  fmt.Println(printaMes(mes))
+}
