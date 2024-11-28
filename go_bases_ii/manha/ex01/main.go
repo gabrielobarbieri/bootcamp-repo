babab4cb2e9a9cb0afefca3f31e74b8b43105360
gabrielobarbieri <gabriel.obarbieri@mercadolivre.com>
2024-11-28 16:29:35 -0300

    Update ex01 tarde

--- go_bases_ii/manha/ex01/main.go
+++ go_bases_ii/manha/ex01/main.go
@@ -3,27 +3,27 @@ package main
 import "fmt"
 
 func imposto(salario float64) float64 {
-  if salario < 50000{
-    return salario
-  } else if salario > 50000 && salario < 150000 {
-    return salario - (salario * 0.17)
-  } else {
-    return salario - (salario * 0.27)
-  }
+	if salario < 50000 {
+		return 0
+	} else if salario > 50000 && salario < 150000 {
+		return salario * 0.17
+	} else {
+		return salario * 0.27
+	}
 }
 
 func main() {
-  var salario float64
+	var salario float64
 
-  salario = 20000
+	salario = 20000
 
-  fmt.Println(imposto(salario))  
+	fmt.Println(imposto(salario))
 
-  salario = 55000
+	salario = 55000
 
-  fmt.Println(imposto(salario))  
+	fmt.Println(imposto(salario))
 
-  salario = 200000
+	salario = 200000
 
-  fmt.Println(imposto(salario))  
+	fmt.Println(imposto(salario))
 }
