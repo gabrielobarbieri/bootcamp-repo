99737ef681866a7bdcb7ddf8539a153d1b2c955d
gabrielobarbieri <gabriel.obarbieri@mercadolivre.com>
2024-11-28 10:58:51 -0300

    ex 1 manha

---
+++ go_bases_ii/manha/ex01/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func imposto(salario float64) float64 {
+  if salario < 50000{
+    return salario
+  } else if salario > 50000 && salario < 150000 {
+    return salario - (salario * 0.17)
+  } else {
+    return salario - (salario * 0.27)
+  }
+}
+
+func main() {
+  var salario float64
+
+  salario = 20000
+
+  fmt.Println(imposto(salario))  
+
+  salario = 55000
+
+  fmt.Println(imposto(salario))  
+
+  salario = 200000
+
+  fmt.Println(imposto(salario))  
+}
